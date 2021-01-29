ffaa75dbaddf44702cb68e80ab0dbd6bfc8315db
onon <gantumuronon539@gmail.com>
2021-01-29 18:43:57 +0800

    first commit

---
+++ public/script.js
@@ -0,0 +1,29 @@
+const socket = io('/')
+var camera = document.getElementById('camera');
+var myScene = document.getElementById('myScene');
+document.addEventListener('keypress', event=>{
+    console.log(camera.getAttribute('position'))
+    socket.emit('moved',{id: socket.id ,pos:camera.getAttribute('position')})
+})
+socket.on('user-joined', (data)=>{
+    if(document.getElementById(data.id) === null){
+        var user_avatar = document.createElement("a-sphere")
+        user_avatar.setAttribute('position', '0 1.6 0')
+        user_avatar.setAttribute('id' , data.id)
+        myScene.append(user_avatar)
+        console.log(data.id)
+
+        socket.emit('user-joined',{id: socket.id ,pos:camera.getAttribute('position')})
+    }else{
+        console.log("user already exist")
+    }
+})
+
+socket.on('moved', (data)=>{
+    var mover = document.getElementById(data.id);
+    mover.setAttribute('position', data.pos);
+})
+
+
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+//var node_modules = require('./node_modules/aframe'); 
+
+var server = require('http').Server(app);
+app.use(express.static('public'))
+app.use(express.static('node_modules'))
+
+//app.use('/node_modules', node_modules)
+var socket = require('socket.io');
+var io = socket(server);
+app.set('view engine', 'ejs')
+io.on('connection', (socket)=>{
+    console.log(socket.id)
+    socket.broadcast.emit('user-joined', {"id":socket.id})
+    socket.on('moved', (data)=>{
+        console.log(data)
+        socket.broadcast.emit('moved', data)
+    })
+
+    socket.on('user-joined', data=>{
+        socket.broadcast.emit('user-joined', data)
+    })
+});
+
+
+
+server.listen(3000, ()=>{
+    console.log('listening on port:3000')
+})
+
