dd00894f4126d8e85342d63dbbc2caf7273390c3
onon <gantumuronon539@gmail.com>
2021-02-09 13:30:14 +0800

    some bug fixes

--- public/login.js
+++ public/login.js
@@ -11,5 +11,10 @@ var join_btn = document.getElementById('join_btn');
 var roomId_txt = document.getElementById('roomId_txt');
 
 join_btn.onclick = function(){
-   window.location.href = '/join?roomId=' + roomId_txt.value + "&uname="+ name_txt.value;
+   var str = name_txt.value;
+   if(!(!str || /^\s*$/.test(str))){
+      var roomId = roomId_txt.value;
+      if(!(!roomId || /^\s*$/.test(roomId)))
+      window.location.href = '/join?roomId=' + roomId_txt.value + "&uname="+ name_txt.value;
+   }
 }
